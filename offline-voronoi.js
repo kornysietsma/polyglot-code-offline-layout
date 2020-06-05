040b807c3e7aae9564e7c6436e52f8f897e4c1ea
Korny Sietsma <korny@sietsma.com>
2020-06-05 13:27:00 +0100

    working with retry loop for flakey voronoi implementation

--- offline-voronoi.js
+++ offline-voronoi.js
@@ -1,7 +1,13 @@
 import { promises as fs, existsSync } from 'fs';
 import yargs from 'yargs';
+import dvm from 'd3-voronoi-map';
+
+const voronoiMapSimulation = dvm.voronoiMapSimulation;
+
+const debug = false;
+
 import d3 from 'd3';
-import vtm from 'd3-voronoi-treemap';
+// import vtm from 'd3-voronoi-treemap';
 
 function computeCirclingPolygon(points, radius) {
   const increment = (2 * Math.PI) / points;
@@ -31,37 +37,153 @@ function pruneWeightlessNodes(hierarchy) {
   }
 }
 
-function deHierarchify(node) {
+function addPaths(pathSoFar, node) {
+  let path;
+  if (pathSoFar === null) {
+    path = ''; // not 'flare' - could use '/' or null - but this is nicer for output
+  } else {
+    if (pathSoFar === '') {
+      path = node.name;
+    } else {
+      path = `${pathSoFar}/${node.name}`;
+    }
+  }
   const children = node.children
-    ? node.children.map((n) => deHierarchify(n))
+    ? node.children.map((n) => addPaths(path, n))
     : undefined;
   return {
-    name: node.data.name,
+    name: node.name,
+    path,
     children: children,
-    polygon: node.polygon,
+    layout: node.layout,
     value: node.value,
-    data: node.data.data,
+    data: node.data,
   };
 }
 
-async function main({ input, output }) {
+function calculate_values(node) {
+  if (node.children) {
+    for (const n of node.children) {
+      calculate_values(n);
+    }
+    const tot = node.children.map((n) => n.value).reduce((a, b) => a + b, 0);
+    node.value = tot;
+  } else {
+    node.value = flareWeightLoc(node);
+  }
+}
+
+function calculateVoronoi(nameSoFar, node, clipPolygon, center) {
+  const name = nameSoFar ? `${nameSoFar}/${node.name}` : node.name;
+  node.layout = {
+    polygon: clipPolygon,
+    center,
+  };
+
+  if (!node.children) {
+    return;
+  }
+  if (debug) {
+    console.log(
+      `calculating map for ${name} with ${node.children.length} children and a clip polygon with ${clipPolygon.length} vertices`
+    );
+  }
+
+  const MY_MAX_COUNT = 500;
+  let metaCount = 0;
+  let metaEnded = false;
+  const META_MAX_COUNT = 100;
+  while (!metaEnded) {
+    try {
+      var simulation = voronoiMapSimulation(node.children)
+        .maxIterationCount(MY_MAX_COUNT)
+        .minWeightRatio(0.01)
+        .weight((d) => d.value)
+        .clip(clipPolygon)
+        .stop();
+
+      var state = simulation.state();
+
+      let tickCount = 0;
+      let warningTime = Date.now();
+      while (!state.ended) {
+        tickCount += 1;
+        const now = Date.now();
+        if (now - warningTime > 10000) {
+          // every 10 seconds
+          warningTime = now;
+          console.log(
+            `slow voronoi processing of ${name} with ${node.children.length} children, tick count: ${tickCount}`
+          );
+        }
+        simulation.tick();
+        state = simulation.state();
+      }
+      if (tickCount === MY_MAX_COUNT) {
+        if (metaCount < META_MAX_COUNT) {
+          metaCount = metaCount + 1;
+          console.log(
+            `processing ${name} with ${node.children.length} children - Exceeded tick count ${tickCount} - retrying from scratch, try ${metaCount}`
+          );
+        } else {
+          console.error('Too many meta retries - stopping anyway');
+          metaEnded = true;
+        }
+      } else {
+        metaEnded = true;
+      }
+    } catch (e) {
+      // re-try from scratch but only after predictable exceptions
+      console.error('caught e', e);
+      if (!e instanceof Error) {
+        console.error('not Error');
+        throw e;
+      }
+      if (e.message == 'bad_polygons' || e.message == 'overweight_loop') {
+        metaCount = metaCount + 1;
+        if (metaCount < META_MAX_COUNT) {
+          console.error(`caught ${e.message}, retrying`, metaCount);
+        } else {
+          console.error(`caught ${e.message}, too many errors!`, metaCount);
+          metaEnded = true; // but fail anyway
+          throw e;
+        }
+      } else {
+        console.error(`unhandled exception ${e.message} - rethrowing`);
+        throw e;
+      }
+    }
+  }
+  var polygons = state.polygons;
+
+  for (const polygon of polygons) {
+    const pdata = polygon.map((d) => d);
+    calculateVoronoi(
+      name,
+      polygon.site.originalObject.data.originalData,
+      pdata,
+      [polygon.site.x, polygon.site.y]
+    );
+  }
+}
+
+async function main({ input, output, points }) {
   const rawData = await fs.readFile(input, 'utf-8');
   const width = 1024;
   const parsedData = JSON.parse(rawData);
   //   console.log(parsedData);
-  console.log('building hierarchy');
-  const rootNode = d3.hierarchy(parsedData).sum(flareWeightLoc);
+  console.log('getting values recursively');
+  calculate_values(parsedData);
   console.log('pruning empty nodes');
-  pruneWeightlessNodes(rootNode);
-  console.log('clipping and building mapper');
-  const clipShape = computeCirclingPolygon(32, width / 2);
+  pruneWeightlessNodes(parsedData);
 
-  const theMapper = vtm.voronoiTreemap().clip(clipShape);
+  // top level clip shape
+  const clipPolygon = computeCirclingPolygon(points, width / 2);
+  const center = [width / 2, width / 2];
 
-  console.log('calculating voronoi treemap');
-  theMapper(rootNode);
-  console.log('converting');
-  const results = deHierarchify(rootNode);
+  calculateVoronoi(null, parsedData, clipPolygon, center);
+
+  const results = addPaths(null, parsedData);
 
   console.log('saving');
   await fs.writeFile(output, JSON.stringify(results, null, 2));
@@ -75,6 +197,10 @@ const argv = yargs
   .describe('i', 'Input JSON file')
   .alias('o', 'output')
   .describe('o', 'Output JSON file')
+  .alias('p', 'points')
+  .number('p')
+  .default('p', 128)
+  .describe('p', 'number of points in the initial bounding circle/polygon')
   .demandOption(['i', 'o'])
   .help('h')
   .alias('h', 'help')
@@ -88,6 +214,7 @@ const argv = yargs
 const args = {
   input: argv.input,
   output: argv.output,
+  points: argv.points,
 };
 
 main(args).then(
